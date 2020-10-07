e207995e55b8885b95287ba60b392e133c5bb213
Oleg Bezhenar <oleg.bezhenar@yahoo.com>
2020-10-07 11:16:51 +0300

    Implemented Linked List.

---
+++ linked-list/linkedlist.py
@@ -0,0 +1,233 @@
+"""Defines Linked List data structure.
+
+The data structure that represents a sequence of nodes. Might be singly linked or
+doubly linked.
+a -> b -> c -> d
+
+  Typical usage example:
+
+  foo = LinkedList()
+  foo.add('a')
+  foo.add('b')
+
+  >> foo
+     ['a', 'b']
+"""
+
+class Node:
+    """Singly linked list node.
+
+    Holds the actual value of the node, and the reference to the next node.
+    If node is the last one, next will be None.
+
+    Attributes:
+        value: Node value.
+        next: Next node.
+    """
+
+    def __init__(self, value = None, next = None):
+        """Initializes Node class with default values.
+
+        Args:
+          value:
+            Optional; Linked List Node value.
+          next:
+            Optional; reference to the next Linked List Node;
+
+        Returns:
+          A new instance of the Linked List Node.
+        """
+        self.value = value
+        self.next = next
+
+    def __str__(self):
+        """Creates string representation of the Node.
+
+        Returns:
+          String representation of the node.
+        """
+        next_str = "Node" if self.next else "None"
+        return f"Node(value = {self.value}, next = {next_str})"
+
+class LinkedList:
+    """Singly Linked List.
+
+    Represents Singly linked list. Contains reference to the list head and
+    number of items.
+
+    Attributes:
+        head: First node of the Linked List.
+        tail: Last node of the Linked List.
+    """
+
+    def __init__(self):
+        """Creates Emply Linked List"""
+        self.head = None
+        self.tail = None
+        self._length = 0
+
+    def __str__(self):
+        """Creates string representation of the list
+
+        Prints the list in the following format:
+          (A) -> (B) -> (C)
+
+        Returns:
+          String representation of the list.
+        """
+        node = self.head
+        output = str(node.value)
+        while node.next != None:
+            node = node.next
+            output += f" -> {str(node.value)}"
+        return output
+
+    def add_last(self, value):
+        """Inserts new node at the end of the list
+
+        Creates node udpates tails, _length and head if required
+
+        Args:
+          value:
+            Linked List Node value.
+        """
+        node = Node(value, None)
+        if self.tail == None:
+            self.tail = node
+            self.head = node
+        else:
+            self.tail.next = node
+            self.tail = node
+        self._length += 1
+
+    def add_first(self, value):
+        """Inserts new node at the beginning of the list.
+
+        Creates node, updates head length and tail if required.
+
+        Args:
+          value:
+            Linked List Node value.
+        """
+        node = Node(value, self.head)
+        self.head = node
+        if self.tail == None:
+            self.tail = self.head
+        self._length += 1
+
+    def add_at(self, value, position: int):
+        """Inserts new node at specific position.
+
+        Args:
+          value: Node value
+          position:New node position
+
+        Rises:
+          IndexError: position is out of the list range.
+        """
+        new = Node(value)
+        if position > self._length:
+            raise IndexError("Position is out of the list range")
+        self._length += 1
+        if position == 0:
+            new.next = self.head
+            self.head = new
+        node = self.head
+        index = 0
+        while node != None:
+            if index + 1 == position:
+                new.next = node.next
+                node.next = new
+                break
+            node = node.next
+            index += 1
+        if new.next == None:
+            self.tail = new
+
+    def first(self):
+        """Retrieves the first element of the Linked List.
+
+        Returns:
+          Value of the first node or None for empty list.
+        """
+        return self.head.value if self.head else None
+
+    def last(self):
+        """Retrieves the last element of the linked list.
+
+        Returns:
+          Value of the last node or none for empty list.
+        """
+        return self.tail.value if self.tail else None
+
+    def delete_first(self):
+        """Removes first element form list
+
+        Set the value of the head to head.next.
+
+        Returns:
+          Value of deleted node or None for empty list.
+        """
+        value = self.head.value if self.head else None
+        self.head = self.head.next if self.head else None
+        if self.head == None:
+            self.tail = None
+        self._length = self._length - 1 if self._length > 0 else 0
+        return value
+
+    def delete_last(self):
+        """Removes last element from list.
+
+        Set the tail to previous element. Takes 0(n) time.
+        For better performance use Doubly Linked List. Here might
+        be optimized if cache before last node.
+
+        Returns:
+          Value of the deleted node or None for empty list.
+        """
+        if self.tail == None:
+            return None
+        value = self.tail.value if self.tail else None
+        node = self.head
+        if node.next == None:
+            node.next = None
+            self.tail = None
+            self.head = None
+            self._length = 0
+            return value
+        while node != None:
+            next_node = node.next
+            if next_node.next == None:
+                node.next = None
+                self.tail = node
+                self._length = self._length - 1 if self._length > 0 else 0
+            node = node.next
+        return value
+
+    def delete_at(self, position: int):
+        """Removes item at specific position.
+
+        Args:
+          position: int, position to remove.
+
+        Returns:
+          Deleted node value.
+
+        Rises:
+          IndexError: position is out of list range.
+        """
+        if position < 0 or position >= self._length:
+            raise IndexError("position is out of list range")
+        if position == 0:
+            return self.delete_first()
+        if position == self._length - 1:
+            return self.delete_last()
+        node = self.head
+        index = 0
+        while node != None:
+            if index + 1 == position:
+                value = node.next.value
+                node.next = node.next.next
+                self._length -= 1
+                return value
+            node = node.next
---
+++ linked-list/test_linkedlist.py
@@ -0,0 +1,345 @@
+import pytest # pylint: disable=import-error
+from linkedlist import LinkedList, Node
+
+# Node
+
+def test_Node_shouldCreateNodeWithDefaultArgs():
+    # act
+    sut = Node()
+
+    # assert
+    assert sut.value == None
+    assert sut.next == None
+
+def test_Node_shouldCreateNodeWithActualArgs():
+    # arrange
+    value = 1
+    next_node = Node()
+
+    # act
+    sut = Node(value, next_node)
+
+    # assert
+    assert sut.value == value
+    assert sut.next == next_node
+
+def test_str_ShouldCreateExpectedString():
+    # arrange
+    next_node = Node(1)
+    node = Node(2, next_node)
+
+    # act
+    node_str = format(node)
+    next_node_str = format(next_node)
+
+    # assert
+    assert node_str == "Node(value = 2, next = Node)"
+    assert next_node_str == "Node(value = 1, next = None)"
+
+# Linked List
+
+def test_LinkedList_ShouldCreateEmptyInstance():
+    # act
+    sut = LinkedList()
+
+    # assert
+    sut.head = None
+    sut._length = 0
+
+def test_LinkedList_firstShouldInsertFirst():
+    # arrange
+    items = ["a", 1, None, True]
+    sut = LinkedList()
+
+    # act
+    sut.add_first(items[0])
+    sut.add_first(items[1])
+    sut.add_first(items[2])
+    sut.add_first(items[3])
+
+    # assert
+    assert sut.head.value == items[3]
+    assert sut.head.next.value == items[2]
+    assert sut.head.next.next.value == items[1]
+    assert sut.head.next.next.next.value == items[0]
+    assert sut.head.next.next.next.next == None
+    assert sut.tail.value == items[0]
+    assert sut._length == 4
+
+def test_LinkedList_lastShouldInsertLast():
+    # arrange
+    items = ["a", 1, None, True]
+    sut = LinkedList()
+
+    # act
+    sut.add_last(items[0])
+    sut.add_last(items[1])
+    sut.add_last(items[2])
+    sut.add_last(items[3])
+
+    # assert
+    assert sut.head.value == items[0]
+    assert sut.head.next.value == items[1]
+    assert sut.head.next.next.value == items[2]
+    assert sut.head.next.next.next.value == items[3]
+    assert sut.head.next.next.next.next == None
+    assert sut.tail.value == items[3]
+    assert sut._length == 4
+
+
+def test_add_at_ShouldInstertElementInTheMiddle():
+    # arrange
+    sut = LinkedList()
+    sut.add_last(1)
+    sut.add_last(3)
+
+    # act
+    sut.add_at(2, 1)
+
+    # assert
+    assert sut.head.value == 1
+    assert sut.head.next.value == 2
+    assert sut.tail.value == 3
+    assert sut._length == 3
+
+def test_add_at_ShouldInsertFirstElement():
+    # arrange
+    sut = LinkedList()
+    sut.add_last(1)
+
+    # act
+    sut.add_at(0, 0)
+
+    # assert
+    assert sut.head.value == 0
+    assert sut.head.next.value == 1
+    assert sut.tail.value == 1
+    assert sut._length == 2
+
+def test_add_at_ShouldInsertLastElement():
+    # arrange
+    sut = LinkedList()
+    sut.add_last(1)
+
+    # act
+    sut.add_at(2, 1)
+
+    # assert
+    assert sut.head.value == 1
+    assert sut.head.next.value == 2
+    assert sut.tail.value == 2
+    assert sut._length == 2
+
+def test_add_at_ShouldInsertSingleElement():
+    # arrange
+    sut = LinkedList()
+
+    # act
+    sut.add_at(1, 0)
+
+    # assert
+    assert sut.head.value == 1
+    assert sut.head.next== None
+    assert sut.tail.value == 1
+    assert sut._length == 1
+
+def test_LinkedList_firstSouldReturNoneIfEmpty():
+    # arrange
+    sut = LinkedList()
+    expected = None
+
+    # act
+    received = sut.first()
+
+    # assert
+    assert expected == received
+
+def test_LinkedList_firstShouldReturnCorrectValue():
+    # arrange
+    sut = LinkedList()
+    expected = 1
+
+    # act
+    sut.add_first(1)
+    received = sut.first()
+
+    # assert
+    assert expected == received
+
+def test_LinkedList_lastSouldReturNoneIfEmpty():
+    # arrange
+    sut = LinkedList()
+    expected = None
+
+    # act
+    received = sut.last()
+
+    # assert
+    assert expected == received
+
+def test_LinkedList_lastShouldReturnCorrectValue():
+    # arrange
+    sut = LinkedList()
+    expected = 3
+
+    # act
+    sut.add_first(1)
+    sut.add_first(2)
+    sut.add_last(3)
+    received = sut.last()
+
+    # assert
+    assert expected == received
+
+def test_delete_first_ShouldDeleteFirstItem():
+    # arrange
+    sut = LinkedList()
+    sut.add_last(1)
+    sut.add_last(2)
+    sut.add_last(3)
+
+    # act
+    deleted_element = sut.delete_first()
+
+    # assert
+    assert sut.head.value == 2
+    assert deleted_element == 1
+    assert sut._length == 2
+
+def test_delete_first_ShouldDeleteOnlyItem():
+    # arrange
+    sut = LinkedList()
+    sut.add_last(1)
+
+    # act
+    deleted_element = sut.delete_first()
+
+    # assert
+    assert sut.head == None
+    assert sut.tail == None
+    assert deleted_element == 1
+    assert sut._length == 0
+
+def test_delete_first_ShouldReturnNoneForEmptyList():
+    # arrange
+    sut = LinkedList()
+
+    # act
+    deleted_element = sut.delete_first()
+
+    # assert
+    assert sut.head == None
+    assert sut.tail == None
+    assert deleted_element == None
+    assert sut._length == 0
+
+def test_delete_last_ShouldRemoveLastItem():
+    # arrange
+    sut = LinkedList()
+    sut.add_last(1)
+    sut.add_last(2)
+    sut.add_last(3)
+
+    # act
+    deleted_element = sut.delete_last()
+
+    # assert
+    assert sut.tail.value == 2
+    assert deleted_element == 3
+    assert sut._length == 2
+
+def test_delete_last_ShouldRemoveOnlyItem():
+    # arrange
+    sut = LinkedList()
+    sut.add_last(1)
+
+    # act
+    deleted_element = sut.delete_last()
+
+    # assert
+    assert sut.tail == None
+    assert sut.head == None
+    assert deleted_element == 1
+    assert sut._length == 0
+
+def test_delete_last_ShouldReturnNoneForEmptyList():
+    # arrange
+    sut = LinkedList()
+
+    # act
+    deleted_element = sut.delete_last()
+
+    # assert
+    assert sut.tail == None
+    assert sut.head == None
+    assert deleted_element == None
+    assert sut._length == 0
+
+def test_delete_at_ShouldRiseIndexErrorWhenInvalidPostion():
+    # arrange
+    sut = LinkedList()
+
+    # act, assert
+    with pytest.raises(IndexError):
+        sut.delete_at(0)
+
+def test_delete_at_ShouldRemoveFirstItem():
+    # arrange
+    sut = LinkedList()
+    sut.add_last(1)
+    sut.add_last(2)
+    sut.add_last(3)
+
+    # act
+    sut.delete_at(0)
+
+    # assert
+    assert sut.head.value == 2
+    assert sut.head.next.value == 3
+    assert sut.tail.value == 3
+    assert sut._length == 2
+
+def test_delete_at_ShouldRemoveLastItem():
+    # arrange
+    sut = LinkedList()
+    sut.add_last(1)
+    sut.add_last(2)
+    sut.add_last(3)
+
+    # act
+    sut.delete_at(2)
+
+    # assert
+    assert sut.head.value == 1
+    assert sut.head.next.value == 2
+    assert sut.tail.value == 2
+    assert sut._length == 2
+
+def test_delete_at_ShouldRemoveMiddleItem():
+    # arrange
+    sut = LinkedList()
+    sut.add_last(1)
+    sut.add_last(2)
+    sut.add_last(3)
+
+    # act
+    sut.delete_at(1)
+
+    # assert
+    assert sut.head.value == 1
+    assert sut.head.next.value == 3
+    assert sut.tail.value == 3
+    assert sut._length == 2
+
+def test_str_ShouldPrintTheList():
+    # arrage
+    sut = LinkedList()
+    sut.add_last(1)
+    sut.add_last(2)
+    sut.add_last(3)
+
+    # act
+    sut_str = format(sut)
+
+    assert sut_str == "1 -> 2 -> 3"
+
